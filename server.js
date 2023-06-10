142dfaed7ce934f73134d7c422c9552cc2e0f007
Sahil Moktan <moktanshail9@gmail.com>
2023-06-10 23:42:18 +0545

    morgan logger working can see all details in console

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+// app.use(morgan("combined")); //gives all details in console
+// app.use(morgan("tiny")); // gives tiny details only
+app.use(morgan(":method:status:url 'HTTP/:http-version'"));
+
+app.get("/", (req, res) => {
+  res.end("morgan logger app");
+});
+
 app.listen(port, () => {
   console.log(`Sever is running on http://localhost${port}`);
 });
